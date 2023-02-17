c3585810a4a95a062a0955c20ca3b259e402a814
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-02-16 18:44:43 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main -  Entry point
+ *
+ *Description: This program will print text using the printf function
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art\n");
+	return (0);
+}
+
