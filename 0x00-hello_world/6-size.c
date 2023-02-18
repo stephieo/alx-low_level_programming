d4cb92e36257331521dcab661768cc95e481cc35
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-02-18 11:47:07 -0800

    pass ABEG

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char:%ld byte(s) \n", (long int) sizeof(a));
-	printf("Size of an int:%ld byte(s) \n", (long int) sizeof(b));
-	printf("Size of a long int:%ld byte(s) \n", (long int) sizeof(c));
-	printf("Size of a long long int:%ld byte(s) \n", (long int) sizeof(d));
-	printf("Size of a float:%ld byte(s) \n", (long int) sizeof(e));
+	printf("Size of a char:%ld byte(s) \n", (unsigned long) sizeof(a));
+	printf("Size of an int:%ld byte(s) \n", (unsigned long) sizeof(b));
+	printf("Size of a long int:%ld byte(s) \n", (unsigned long) sizeof(c));
+	printf("Size of a long long int:%ld byte(s) \n", (unsigned long) sizeof(d));
+	printf("Size of a float:%ld byte(s) \n", (unsigned long) sizeof(e));
 	return (0);
 }
 
