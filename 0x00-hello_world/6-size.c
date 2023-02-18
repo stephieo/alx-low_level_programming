6aea57f8bea90362e62e21cc543ae84bcc9b143e
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-02-18 08:38:54 -0800

    task 6, typo fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s) \n", (long int) sizeof(a));
 	printf("Size of an int: %ld byte(s) \n", (long int) sizeof(b));
-	printf("Size of an long int: %ld byte(s) \n", (long int) sizeof(c));
-	printf("Size of an long long int: %ld byte(s) \n", (long int) sizeof(d));
-	printf("Size of an long long int: %ld byte(s) \n", (long int) sizeof(e));
+	printf("Size of a long int: %ld byte(s) \n", (long int) sizeof(c));
+	printf("Size of a long long int: %ld byte(s) \n", (long int) sizeof(d));
+	printf("Size of a long long int: %ld byte(s) \n", (long int) sizeof(e));
 }
 
