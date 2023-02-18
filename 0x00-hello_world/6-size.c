aac2b704e887b0d153d03770b05edc80e78900a8
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-02-18 08:48:41 -0800

    task 6, removed whitespace

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %ld byte(s) \n", (long int) sizeof(a));
-	printf("Size of an int: %ld byte(s) \n", (long int) sizeof(b));
-	printf("Size of a long int: %ld byte(s) \n", (long int) sizeof(c));
-	printf("Size of a long long int: %ld byte(s) \n", (long int) sizeof(d));
+	printf("Size of a char:%ld byte(s) \n", (long int) sizeof(a));
+	printf("Size of an int:%ld byte(s) \n", (long int) sizeof(b));
+	printf("Size of a long int:%ld byte(s) \n", (long int) sizeof(c));
+	printf("Size of a long long int:%ld byte(s) \n", (long int) sizeof(d));
 	printf("Size of a float:%ld byte(s) \n", (long int) sizeof(e));
 }
 
