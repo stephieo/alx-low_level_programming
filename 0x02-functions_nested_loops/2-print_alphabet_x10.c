a8d39dccf09331d2801a024b3233593a594803af
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-02-23 09:33:23 -0800

    task 2- errors fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * print_alphabet-x10 - function for header
+ * print_alphabet_x10 - function for header
  *
  * Descrioption:print  alphabet ten times
  *
  * Return: void
  */
-void print_alphabet_x10(void) /*this will run 10 times*/
+void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		char i;
 
 		for (i = 'a' ; i <= 'z' ; i++)
 		{
 			_putchar(i);
-		} 
+		}
 		_putchar('\n');
 	}
 }
