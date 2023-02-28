8c42460e80f7d09bb8eeeaee29f26187151afec3
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-02-28 07:38:36 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,20 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (0);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
 	}
 
-	return (0);
+	if (n < 0 || n == 0)
+		return (0);
+	else
+		return (1);
+
 }
 
