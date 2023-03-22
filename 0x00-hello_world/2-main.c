4b7929c9282a7b5e4fb398322e8dfac23fbd2f64
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-03-22 19:30:35 +0000

    tasks 2,3 and 5

---
+++ 0x00-hello_world/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x00-hello_world/3-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Returns : Always 0 (success)
+ */
+int main(void)
+{
+	return (0);
+}
