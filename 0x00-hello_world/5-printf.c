649af039c6609a4872ebcbe090d968c7efb14140
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-03-22 19:35:22 +0000

    Fixed output error and betty-doc error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main- entry point
  *
- * Returns: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
