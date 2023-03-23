4eebf51798a1118ca2d079bc5ca7b73e9c00e80d
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-03-23 18:04:55 +0000

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints  a string using puts function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
