0bd64f874923ab4baeccbf3376b2700c01779e98
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-03-28 15:32:16 +0000

    task 9: removed seperator from last output

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,11 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 		putchar('\n');
 	return (0);
