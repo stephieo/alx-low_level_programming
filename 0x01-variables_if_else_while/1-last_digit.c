def94a43a41c585393de321fa8865518968eec02
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-03-28 00:07:59 +0000

    task 1 fixed compile error(undeclared variable)

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 	int n;
-	int lastdigit;
+	int lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	last = n % 10;
+	lastd = n % 10;
 	if (n > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
