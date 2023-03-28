7bfeafb1d7487897c8ead7533c3217b58b91c51a
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-03-28 18:19:07 +0000

    task 6: fixed betty errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main -entry point
  *
- * Description: prints the size of various types ont the computer it is compiled on
+ * Description: using sizeof for different compiler environments
  *
  * Return: always 0 (success)
  */
@@ -14,10 +14,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n",(unsigned long) sizeof(a));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c)); 
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 		return (0);
