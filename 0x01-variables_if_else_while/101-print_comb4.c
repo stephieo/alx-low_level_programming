89251b85aeee56906ad99d35700b82f978f7d3d0
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-03-29 16:48:08 +0000

    tasks 11 and 12: creating more complex combinations with loops and control statements

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Description: printing all combinations of three digits without repitition
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48 ; a < 58 ; a++)
+	{
+		for (b = 48 ; b < 58 ; b++)
+		{
+			if (b != a && b >a)
+			{
+				for(c = 48 ; c < 58 ; c++)
+				{
+					if ( c != b && c > b)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(c);
+						if ((a + b + c) != 168)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main -  entry point: print all combinations of two 2-digit numbers
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x, y;
+	int a, b, c, d;
+
+	for (x = 0; x < 100; x++)
+	{
+		a = (x / 10) + 48;
+		b = (x % 10) + 48;
+
+		for (y = 0; y < 100; y++)
+		{
+			c = (y / 10) + 48;
+			d = (y % 10) + 48;
+
+			if ((d != 48 && d > b) && a <= c)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(' ');
+				putchar(c);
+				putchar(d);
+
+				if (!(a == 57 && b == 56))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
