ca13a331e5a1d8b62bbcf1cd62a213bc30a235cd
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-01 20:31:52 +0000

    task 2 : refactored code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char j;
 
 	for (i = 0; i <= 10; i++)
 	{
-		print_alphabet();
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
 }
