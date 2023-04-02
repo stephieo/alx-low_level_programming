a486a5da28ae411b78f081a87212be243221a3f2
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-02 00:29:38 +0000

    task 7- incomplete

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit -returns the value of the last digit of an integer
+ * @int - integer to be evaluated
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+	if (last_digit < 0);
+		_putchar((last_digit * (-1)) + '0');
+		return((last_digit *(-1)) +'0');
+	else 
+		_putchar(last_digit + '0');		
+		return (last_digit);
+}
