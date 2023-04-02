628cbb70e7737c848556e0e810f986f724eb519a
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-02 23:50:41 +0000

    task 14: fibonacci sequence from 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 fibonacci numbers
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	long int f[50];
+
+	int i;
+
+	f[0] = 1;
+	f[1] = 2;
+	printf("%ld, ", f[0]);
+	printf("%ld, ", f[1]);
+	i = 2;
+	while (i < 50)
+	{
+		f[i] = f[i - 1] + f[i - 2];
+		printf("%ld, ", f[i]);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
