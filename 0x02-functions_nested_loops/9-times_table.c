839df87fd6984a027bcb9453c38e82aeda70c781
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-02 16:40:18 +0000

    task 9: prints out the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * times_table - prints out the  9 times table
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	a = 0;
+	while (a < 10)
+	{
+		b = 0;
+		while (b < 10)
+		{
+			c = a * b;
+			if (c % 10 == c && b == 9)
+			{
+				_putchar(c + 48);
+				_putchar('\n');
+			}
+			else if (c % 10 == c && b != 9)
+			{
+				_putchar(c + 48);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (c / 10 > 0 && b == 9)
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+				_putchar('\n');
+			}
+			else if (c / 10 > 0 && b != 9)
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+			b++;
+		}
+	_putchar('\n');
+		a++;
+	}
+	_putchar('\n');
+}
+
