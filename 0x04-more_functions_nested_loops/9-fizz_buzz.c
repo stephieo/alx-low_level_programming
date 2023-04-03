0c88a563e52c958a2ab28a7a1b4b1488d9495fef
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-03 23:05:05 +0000

    task 9: changed printing order

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,25 +6,27 @@
  */
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	printf("%d", i);
+
+	for (i = 2; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if ((i % 3) == 0 && (i % 5) != 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if ((i % 3) != 0 && (i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 
 	}
