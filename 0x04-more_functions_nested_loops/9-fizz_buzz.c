0ec01d35468dbf528bbb95acaedb328569c65fe0
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-03 22:16:15 +0000

    task 9 : FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- FizzBuzz test
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0 && (i % 5) != 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 3) != 0 && (i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+
+	}
+
+	printf("\n");
+	return (0);
+}
+
