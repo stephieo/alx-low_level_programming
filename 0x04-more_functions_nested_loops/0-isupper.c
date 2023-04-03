f6ed1cbc4ac0ec37de1a96bceff1021aa175c031
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-03 17:06:49 +0000

    task 0: checks for uppercase characters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper -checks for upper case character
+ * @c: ascii code of character
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c >= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ more practice on  functions and nested loops
