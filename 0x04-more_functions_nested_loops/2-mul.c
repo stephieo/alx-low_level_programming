b96c174c86dcd78506db2c0ad015c4887d65c3d9
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-03 19:01:30 +0000

    task 2: multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
