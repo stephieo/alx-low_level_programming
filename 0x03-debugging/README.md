d64f31f5f28c52d2fc54320a028f26455e458b2e
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-03 13:36:49 +0000

    task 2 : changed  assignment conditions

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+ this project is for learning  and practicing debugging code
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void largest_number(int, int, int);
+
+#endif /* MAIN_H */
+
