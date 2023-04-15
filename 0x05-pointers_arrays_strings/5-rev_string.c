324adf86c2d3902ec37f2740d02c5ac79657bcb1
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-15 14:52:14 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string- reverses a string
+ * @s: pointer to original string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int temp, i, last;
+	int length = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		length += 1;
+	}
+
+	last = length - 1;
+
+	for (i = 0; i < last; i++)
+	{
+		temp = s[i];
+		s[i] = s[last];
+		s[last] = temp;
+		last--;
+	}
+}
+
