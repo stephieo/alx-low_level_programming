d50890d3f5ee1a714e550ac2c91991c6048b3186
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-18 13:59:25 +0000

    task 2:printing a struct

--- 0x0E-structures_typedef/test.c
+++ 0x0E-structures_typedef/test.c
@@ -8,9 +8,10 @@
 int main(void)
 {
 	struct dog my_dog;
-	
-	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
-	printf("My name is %s, I am %.2f, and my owner is %s\n",
-		my_dog.name, my_dog.age, my_dog.owner);
+
+	/*my_dog.name = "Poppy";*/
+	my_dog.age = 3.5;
+	my_dog.owner = "Beth";
+	print_dog(&my_dog);
 	return (0);
 }
