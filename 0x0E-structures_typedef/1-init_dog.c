665416ee6afc32923417b4f5fc951172603c10ea
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-04-18 01:33:39 +0000

    tasks 0 and 1: creating and initializing structs

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog -initializes variable of type struct dog
+ * @d:pointer to struct of type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	user = malloc(sizeof(struct dog));
+	if (dog != NULL)
+	{
+		d->name = name;
+		d->email = email;
+		d->age = age;
+
+	}
+}
