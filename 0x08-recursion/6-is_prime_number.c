40b7c273a0e967987fce545156ee026754ffd2e2
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-07 12:13:50 +0000

    functions displaying use of recursions

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: number to be checked
+ *
+ * Return: 1 if a prime number, 0 if not.
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (0);
+
+	x = n -1;
+	return (looper(x,n));
+
+}
+
+/**
+ * looper - checks if a number is divisible by any number before it
+ * @x: the divisor
+ * @n: the  dividend
+ *
+ * Return: 0 if x is 1 or if n is divisible by x, 1 if  is prime
+ */
+int looper(int x, int n)
+{
+	if (x > 1)
+	{
+		if (n % x == 0)
+		{
+			return (0);
+		}
+		else
+		{
+	
+			return (looper(x-1,n);
+		}
+	}
+	else if (x <= 1 || )/* switch this to the top*/
+	{
+		return (0);
+	}
+
+
+}
