ed8000478528463c3a72bcdb4b26bca378dce4a0
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-07 21:57:20 +0000

    task 100: fixed recursion error in helper function to obtain correct value of last index

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+ * getlength - gets last index of a string
+ * @s: string to measure
+ * @m: starting index
+ *
+ * Return: last index of string
+ */
+int getlastindex(char *s, int m)
+{
+	int length;
+	
+	if(s[m])
+	{
+		length = getlastindex(s, m + 1);
+		return (length);
+	}
+	else
+	{
+		return (m - 1);
+	}
+}
+
+/**
  * checker - compares characters in a string
  * @s: string to be used
  * @forward: index to the first character
@@ -7,13 +29,13 @@
  *
  * Return: 0 if letters are not equal,  1 if all letters are compared and equal
  */
-int checker (char *s, int forward, int backward)
+int checker(char *s, int forward, int backward)
 {
 	if (s[forward] != s[backward])
 	{
 		return (0);
 	}
-	if ( forward >= backward && s[forward] ==s[backward])
+	if (forward >= backward && s[forward] == s[backward])
 	{
 		return (1);
 	}
@@ -22,7 +44,8 @@ int checker (char *s, int forward, int backward)
 		return (checker(s, forward + 1, backward - 1));
 	}
 }
-	
+
+
 /**
  * is_palindrome -  checks if a string is a palindrome
  * @s: string to be checked
@@ -31,14 +54,11 @@ int checker (char *s, int forward, int backward)
  */
 int is_palindrome(char *s)
 {
-	int i;
-
-	for ( i = 0; s[i] != '\0'; i++)
-	{
-		;
-	}
-
+	int result;
+	int i = 0;
 	
-	return (checker (s, i - i, i -1));
+	result = getlastindex(s, i);
+
+	return (checker(s, i, result));
 }
 
