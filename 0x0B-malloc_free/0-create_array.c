4dc91a7dd3fc835d7fe9973f6640eafcc2ed990f
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-12 21:04:01 +0000

    created 1 dimensional array with malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+/**
+ * create_array - creates an array using malloc
+ * @size: desired size of array
+ * @c: character to be used for initialization
+ *
+ * Return: null (size is 0 or malloc returns NULL) , pointer to array (succes)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+
+	arr = (char *)malloc(size * sizeof(char));
+
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+
+	return (arr);
+}
