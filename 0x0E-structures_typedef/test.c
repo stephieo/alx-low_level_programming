8f9d55dd7a99e3673698d9f7c01362fcaf4d3731
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-19 20:32:48 +0000

    adjusted task 2 with test case

--- 0x0E-structures_typedef/test.c
+++ 0x0E-structures_typedef/test.c
@@ -8,10 +8,16 @@
  */
 int main(void)
 {
-	dog_t *my_dog;
+	/*dog_t *my_dog;
 
 	my_dog = new_dog("Poppy", 3.5, "Bob");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-	free_dog(my_dog);
+	free_dog(my_dog);*/
+
+	struct dog my_dog;
+		my_dog.name = "poppy";
+		my_dog.age = 3.5;
+		my_dog.owner = "bob";
+		print_dog(&my_dog);
 	return (0);
 }
