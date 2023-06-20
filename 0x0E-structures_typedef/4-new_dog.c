d430c102d415b3ea4ce404473023ac62e5280146
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-20 15:01:59 +0000

    memory leak handling

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,43 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * new_dog- function that creates a new dog
+ * inputlen - gets lengthof string input
+ * @s: pointer to string
+ *
+ * Return: length of string
+ */
+int inputlen(char *s)
+{
+	int i = 0, length = 0;
+
+	while (s[i++])
+		length++;
+	return (length);
+}
+
+/**
+ * _strcpy - copoies string from source to destination
+ * @src: pointer to source
+ * @dest: pointer to destination string
+ *
+ * Return:pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - function that creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -11,6 +47,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	int len_n = 0, len_o = 0;
 
 	new =  malloc(sizeof(dog_t));
 
@@ -20,9 +57,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new->name = name;
+	len_n = inputlen(name);
+	new->name = malloc(len_n + 1);
+	if (new->name == NULL)
+	{
+		free(new);
+	}
+	_strcpy(new->name, name);
+
 	new->age = age;
-	new->owner = owner;
+
+	len_o = inputlen(owner);
+	new->owner = malloc(len_o + 1);
+
+	if (new->owner == NULL)
+	{
+		free(new);
+		free(new->name);
+	}
+	_strcpy(new->owner, owner);
 
 	return (new);
 }
