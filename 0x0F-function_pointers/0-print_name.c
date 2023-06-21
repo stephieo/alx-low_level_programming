a3a1c46f200e74429a358b1b9b603860333de011
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-21 10:41:28 +0000

    function that prints a name in various ways

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+/**
+ * print_name - function that prints a name in different ways
+ * @name: name to print
+ * @f: pointer to specific function
+ * 
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+	return;
+}
+
