be836eccb6a13ed9e0dc4eb36e4f444cc7cf19e9
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-21 12:16:55 +0000

    function to carry out action on all elements of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+/**
+ * array_iterator - executes a function on every element of an array
+ * @array: pointer to array
+ * @size: size of array
+ * @action: function to be executed on elements
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+
+	for(i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
