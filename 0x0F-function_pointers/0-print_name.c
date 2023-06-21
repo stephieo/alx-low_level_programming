862357dba33e3636458c0d79e72f4da268483ad9
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-21 12:27:28 +0000

    fixed betty coding style error

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
  * print_name - function that prints a name in different ways
  * @name: name to print
  * @f: pointer to specific function
- * 
+ *
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
