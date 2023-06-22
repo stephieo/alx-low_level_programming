a54979e0e097a7785debee40a1ab159b09815b59
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-22 23:09:32 +0000

    adjusted flow of code

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,11 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list operands;
 
-	va_start(operands, n);
 	if (n == 0)
 	{
 		return (0);
 	}
+
+	va_start(operands, n);
 	
 	while (i < n)
 	{
