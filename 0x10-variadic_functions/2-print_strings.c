cdcc350ac33ee525fbbbda9c7af268224a5c2590
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-23 02:02:32 +0000

    created functio to print variable number of strings; included condition for printing seperator( task 1 & 2)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,3 +1,29 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints variable number of strings
+ * @seperator: character(s) to seperate each string
+ * @n: number of arguments 
+ */
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *string = NULL;
+	va_list strings;
 
+	va_start(strings, n);
+	while (i < n)
+	{
+		string = va_arg(strings, char *);
+		if (string == NULL)
+			printf("(nil)");
+		else
+			printf("%s", string);
+		
+		if (seperator != NULL && i != (n - 1))
+			printf("%s", seperator);
+		i++;
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/test.c
+++ 0x10-variadic_functions/test.c
@@ -12,6 +12,7 @@ int main(void)
 	sum = sum_them_all(2, -3, 7);
 	printf("%d\n", sum);
 	print_numbers(", ", 2, -9, -29);
+	print_strings(", ", 2, "Jay","Django");
 
 
 	return(0);
