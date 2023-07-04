09439ecee1e6f4d722d6bcfc30bb41e1bb305e46
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-07-04 00:26:42 +0000

    created constructor function first()

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * first - this code will be executed before main
+ */
+void first(void)__attribute__((constructor))__
+{
+	printf("You're beat! and yet you must allow,\nI boremy house upon my back!\n");
+}
