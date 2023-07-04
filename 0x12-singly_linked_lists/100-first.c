0a108165d3a2d051f588388ed8d068067b9fd448
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-07-04 18:59:44 +0000

    created function add_node_end()

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,5 +4,6 @@
  */
 void __attribute__((constructor))first(void)
 {
-	printf("You're beat! and yet you must allow,\nI boremy house upon my back!\n");
+	printf("You're beat! and yet you must allow,
+			\nI boremy house upon my back!\n");
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,34 @@
 #include "lists.h"
 /**
+ * add_node_end - adds  a node at the end of linked list
+ * @head: pointer to head of linked list
+ * @str: string to be placed in new node
  *
+ * Return: address of new node or NULL
  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp = NULL;
+	size_t i = 0;
+	list_t *count = **head;
+
+
+	temp = malloc(sizeof(list_t));
+
+	if (temp == NULL)
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	while (str[i] != '\0')
+		i++;
+	temp->len = i;
+
+	while (*count != NULL)
+		count = count->next;
+
+	count->next = temp;
+
+
+	return (temp);
+}
