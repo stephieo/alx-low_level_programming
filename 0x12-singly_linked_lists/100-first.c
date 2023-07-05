9bd4c9af5d746a05e57ad5f4a7df9359c26eb9f5
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-07-05 12:06:03 +0000

    updated 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,5 +4,5 @@
  */
 void __attribute__((constructor))first(void)
 {
-	printf("You're beat! and yet you must allow,i\nI boremy house upon my back!\n");
+	printf("You're beat! and yet you must allow,\nI bore my house upon my back!\n");
 }
