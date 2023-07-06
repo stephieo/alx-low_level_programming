e52f72fae0010aaa914876c1e9c44a64c3939c43
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-07-06 16:24:22 +0000

    created 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts node at given position
+ * @idx: index of lst where node should be added
+ * @head: pointer to first node
+ * @n: data in new node
+ *
+ * Return: pointer to  new node or NULL
+ */
+insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = *head;
+	listint_t *create = *head;
+	int i;
+
+	create = malloc(sizeof(listint_t));
+	if (create == NULL)
+		return (NULL);
+
+	create->n = n;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		if (temp == NULL)
+			return (NULL);
+		temp = temp->next;
+	}
+
+	create->next = temp->next;
+	temp->next = create;
+
+	return (create);
+
+}
