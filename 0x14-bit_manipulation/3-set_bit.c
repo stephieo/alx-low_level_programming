02a8d2d3016fbe6b4027d83d58f79c10bad72eb2
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-07-09 06:59:15 +0000

    removed debuggging lines

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,8 +14,6 @@ int set_bit (unsigned long int *n, unsigned int index)
 		return (-1);
 
 	mask = 1 << index;
-	printf("mask is %lu\n", mask);
-	printf("number is %lu\n", *n);
 
 		*n = *n | mask;
 		return (1);
