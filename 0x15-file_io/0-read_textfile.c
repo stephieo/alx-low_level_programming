104347230fb628264fc65882f14ffbf1f6df9174
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-07-11 11:10:06 +0000

    fixed betty errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,30 +9,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t writecount =1, readcount = -1;
+	ssize_t writecount = 1, readcount = -1;
 	int newfd, checkclose;
 	void *mybuff;
 
 	if (filename == NULL)
 		return (0);
-	
+
 	mybuff = malloc(sizeof(char) * 1024);
-	if(mybuff == NULL)
-		return(-1);
-	
-	newfd = open(filename, O_RDWR | O_CREAT , S_IRWXU | S_IROTH | S_IXOTH);
+	if (mybuff == NULL)
+		return (-1);
+
+	newfd = open(filename, O_RDWR | O_CREAT, S_IRWXU | S_IROTH | S_IXOTH);
 	if (newfd == -1)
 		return (0);
 
-	readcount = read(newfd, mybuff, letters);	
+	readcount = read(newfd, mybuff, letters);
 	if (readcount == -1)
 		return (0);
 
-	writecount = write( STDOUT_FILENO, mybuff, letters);
+	writecount = write(STDOUT_FILENO, mybuff, letters);
 	if (writecount == -1)
 		return (0);
-	
-	checkclose = close (newfd);
+
+	checkclose = close(newfd);
 	if (checkclose == -1)
 		return (0);
 
