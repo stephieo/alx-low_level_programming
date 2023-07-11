ed8f67dff218501b5baacd70774a8d19f9b7b834
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-07-11 11:03:53 +0000

    updated0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,18 +9,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t readcount;
-	int newfd;
-
-	if (fd == -1)
-		return (0);
-	
-	readcount = read(filename,O_RDWR | O_CREAT , S_IWRXU | S_IROTH | S_IXOTH);
+	ssize_t writecount =1, readcount = -1;
+	int newfd, checkclose;
+	void *mybuff;
 
 	if (filename == NULL)
 		return (0);
+	
+	mybuff = malloc(sizeof(char) * 1024);
+	if(mybuff == NULL)
+		return(-1);
+	
+	newfd = open(filename, O_RDWR | O_CREAT , S_IRWXU | S_IROTH | S_IXOTH);
+	if (newfd == -1)
+		return (0);
 
-	if (write == -1)
+	readcount = read(newfd, mybuff, letters);	
+	if (readcount == -1)
+		return (0);
+
+	writecount = write( STDOUT_FILENO, mybuff, letters);
+	if (writecount == -1)
+		return (0);
+	
+	checkclose = close (newfd);
+	if (checkclose == -1)
 		return (0);
 
 	return (readcount);
