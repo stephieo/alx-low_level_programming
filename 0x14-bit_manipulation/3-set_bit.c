7423def3dba5c5bf98a71f782ce08a4da173bb99
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-08-04 01:00:07 +0000

    created function to set a bit in a binary

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * set_bit - sets the value of a bit at an index to 1
+ * @n: pointer to binary number
+ * @index: desired index to set
+ *
+ * Return: 1 (success), -1 (error)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (n == NULL || index > (sizeof(unsigned long int) * 8) - 1)
+		return (-1);
+
+	mask = 1 << index;
+
+		*n = *n | mask;
+		return (1);
+}
