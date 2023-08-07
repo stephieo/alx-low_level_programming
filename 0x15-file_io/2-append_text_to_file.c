385c69b161486f2a47d6836a1a2b73fc21491fff
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-08-07 17:07:52 +0000

    task 2: function to append text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * append_text_to_file - creates a file
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1(succes), -1 (any function error)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, fd, writecheck;
+	size_t length;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+
+	if (text_content != NULL)
+	{
+		writecheck = write(fd, text_content, length);
+		if (writecheck == -1)
+			return (-1);
+
+	}
+	else
+	{
+		return (-1);
+	}
+	return (1);
+
+}
