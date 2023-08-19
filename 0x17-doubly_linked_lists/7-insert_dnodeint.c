82c37cc18d32b59d72c27713443db8fbbb43d771
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-08-19 19:56:56 +0000

    task 7 -2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,9 +19,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new);
 	}
 
-	length = dlistint_len(*h);
-	if (idx >= length)
-		return (NULL);
 
 	while (i < idx)
 	{
