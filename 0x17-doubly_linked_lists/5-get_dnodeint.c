3fdf334faa7b12f7e2412c3159f3374c02b26691
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-08-19 18:52:27 +0000

    task 5 update

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,15 +11,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *temp = head;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	while (i < index)
 	{
-		if (temp == NULL)
-			break;
-
 		temp = temp->next;
 		i++;
-	
 	}
 
 	return (temp);
-
+}
