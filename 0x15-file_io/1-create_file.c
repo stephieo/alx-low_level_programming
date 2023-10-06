258288ca40aa7dea2d1a00e6e8bab2127036e59e
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-10-06 12:42:48 +0000

    solution to task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,23 +13,23 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename,  O_CREAT | O_RDWR | O_TRUNC, S_IRUSR & S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i] != '\0')
-	{
-		length++;
-		i++;
-	}
+	if (text_content == NULL)
+		text_content =  " ";
+	else
+		for (i = 0, length = 0; text_content[i] != '\0'; i++, length++)
+			;
 
-	if (text_content != NULL)
-	{
-		writecheck = write(fd, text_content, length);
-		if (writecheck == -1)
-			return (-1);
 
-	}
+	writecheck = write(fd, text_content, length);
+	if (writecheck == -1)
+		return (-1);
+
+
+	close(fd);
 	return (1);
 
 }
