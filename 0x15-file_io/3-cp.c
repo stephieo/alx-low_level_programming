56c4b5d54c529d30147b544b1240f0c5d3ebb005
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-10-16 13:53:20 +0000

    update to tak 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,41 @@
 #include "main.h"
 /**
+ * errochecker - checks for errors in open, read, write and close functions
+ * @original: return value for functions acting on original copy
+ * @copy: return valure for functions acting on destination file
+ * @filename: name of file to check
+ * @fd: fd of file being checked
+ * @function: code of function being checked
+ */
+void errorchecker(int original, int copy, char *filename,
+			int fd, char function)
+{
+	if (function != 'c')
+	{
+		if (original == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", filename);
+			exit(98);
+		}
+
+		if (copy== -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", filename);
+			exit(99);
+		}
+	}
+	else
+	{
+		if(original == -1  || copy == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", fd);
+			exit(100);
+		}
+	}
+	
+}
+
+/**
  * main - program that copies the content of a file to another file
  * @ac: argument count
  * @av: argument array
@@ -9,9 +45,8 @@
 int main(int ac, char **av)
 {
 	ssize_t readcheck = 1024, writecheck;
-	size_t bytesread = 1024;
 	int filefrom_fd, fileto_fd, closecheck1, closecheck2;
-	char *mybuff, *testbuff;
+	char mybuff[1024];
 
 	if (ac != 3)
 	{
@@ -19,60 +54,33 @@ int main(int ac, char **av)
 		exit(97);
 	}
 
-	/*malloc buffer*/
-	mybuff = malloc(sizeof(char) * 1024);
+	/*mybuff = malloc(sizeof(char) * 1024);
 	if (!mybuff)
 	{
 		write(STDERR_FILENO, "malloc error\n", 13);
 		exit(-1);
-	}
-	testbuff = malloc(sizeof(char) * 1024);
+	}*/
 
-	/*open file from*/
 	filefrom_fd = open(av[1], O_RDONLY);
-	/*open file to */
 	fileto_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	errorchecker(filefrom_fd, fileto_fd, av[1], 0, 'o');
+	errorchecker(filefrom_fd, fileto_fd, av[2], 0, 'o');
 
-		if (filefrom_fd == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-		if (fileto_fd == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
-	while (readcheck == 1024)
+	while (readcheck != 0)
 	{
-		/*read file from to buffer*/
-		readcheck = read(filefrom_fd, mybuff, bytesread);
-
+		readcheck = read(filefrom_fd, mybuff, sizeof(mybuff));
 		if (readcheck == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-		printf("%s\n", mybuff);
+			errorchecker(-1, 0, av[1], 0, 'r');
+	/*	printf("%s\n 1\n\n", mybuff);*/
 
-		/*write from buffer to fileto*/
-		writecheck = write(fileto_fd, mybuff, bytesread);
+		writecheck = write(fileto_fd, mybuff, readcheck);
 		if (writecheck == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
-		read(fileto_fd, testbuff, bytesread);
-		printf("%s", testbuff);
+			errorchecker(0, -1, av[2], 0, 'w');
 	}
-	/*close files*/
-	closecheck1 = close(fileto_fd);
-	closecheck2 = close(filefrom_fd);
-	if (closecheck1 == -1 || closecheck2 == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", closecheck1 ? fileto_fd : filefrom_fd);
-		exit(100);
-	}
+	closecheck1 = close(filefrom_fd);
+	errorchecker(closecheck1, 0, av[1], fileto_fd, 'c');
 	
+	closecheck2 = close(fileto_fd);
+	errorchecker(0, closecheck2, av[2], fileto_fd, 'c');
 	return (0);
 }
