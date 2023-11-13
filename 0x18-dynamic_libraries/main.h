a9d5c28d241f856c022448c798429ef24eeabbf5
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-13 13:15:10 +0000

    betty error fixes

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,4 +1,4 @@
-#ifndef  MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
@@ -11,19 +11,20 @@ int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char * dest, char *src);
-char *_strncat(char * dest, char *src, int n);
-char *_strncpy(char * dest, char *src, int n);
-int _strcmp(char *s1, char * s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *s, char *b, unsigned int n);
 char *_strchr(char *s, char *c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char * haystaclk, char *needle);
+char *_strstr(char *haystaclk, char *needle);
 
-
-
+/**
+ * functions from memset downeard still need to be written properly,
+ *  its buggy placeholder code thats there
+ */
 #endif /* MAIN_H */
 
-/*functions from memset downeard still need to be written properly, its buggy placeholder code thats there*/
