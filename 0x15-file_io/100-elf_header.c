79ecda8d0e09ce8e141ea66e613febd2228fa3c0
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-16 06:06:45 +0000

    0x15 test files

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <elf.h>
+
+/**
+ * main - program to read elf files
+ * @ac: argument count
+ * @av: argument vector list
+ *  
+ *  Return: 0 (success)
+ */
+int main(int ac, char **av)
+{
+
+}
