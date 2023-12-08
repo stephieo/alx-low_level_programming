e0a62a73dcc19f88c9acbee048c9dffb0b671c9c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-08 10:05:28 +0000

    feat: add solution to task 1

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,4 +40,5 @@ typedef struct hash_table_s
 
 /** FUNCTION PROTOTYPES **/
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 #endif /*MAIN.H*/
