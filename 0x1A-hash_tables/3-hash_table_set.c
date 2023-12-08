436d9d229ab39c795e73d6d9bb27a6172f982e1a
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-08 13:59:55 +0000

    style: betty style fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int ht_index;
 	hash_node_t *new_element;
+
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	new_element = malloc(sizeof(hash_node_t));
